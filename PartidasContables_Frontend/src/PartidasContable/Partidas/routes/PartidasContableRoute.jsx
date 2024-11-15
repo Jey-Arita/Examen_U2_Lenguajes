5e64239fa7842eda1f9c27c45e633d2db1847430
MichaelGald <michaelcom292@gmail.com>
2024-11-14 22:23:45 -0600

    log-descomentados-parte-2

--- PartidasContables_Frontend/src/PartidasContable/Partidas/routes/PartidasContableRoute.jsx
+++ PartidasContables_Frontend/src/PartidasContable/Partidas/routes/PartidasContableRoute.jsx
@@ -3,13 +3,15 @@ import { CatalogoCuenta, CrearPartidas, Footer, InicioPage, LogPages} from "../p
 
 export const PartidasContableRouter = () => {
     return (
-      <div className="overflow-x-hidden bg-white w-screen h-screen bg-hero-pattern bg-no-repeat bg-cover">
+      <div className="flex flex-col min-h-screen bg-white w-screen overflow-x-hidden bg-hero-pattern bg-no-repeat bg-cover">
+        <div className="flex-grow">
             <Routes>
                 <Route path="/catalogo-cuenta" element={<CatalogoCuenta />} />
                 <Route path="/crear-partidas" element={<CrearPartidas/>}/>
                 <Route path="/inicio" element={<InicioPage/>}/>
                 <Route path="/logs" element={<LogPages/>}/>
             </Routes>
+            </div>
             <footer className="mt-auto">
               <Footer/>
             </footer>
